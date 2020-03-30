8ac7148dfbf32e40e76374ef3a6594d126f79977
Petri Hoskonen <route.electrique@gmail.com>
2020-03-30 06:05:12 +0300

    Enemies take damage.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float health = 100;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        ProcessHit(damageDealer);
+    }
+
+    private void ProcessHit(DamageDealer damageDealer)
+    {
+        health -= damageDealer.GetDamage();
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
