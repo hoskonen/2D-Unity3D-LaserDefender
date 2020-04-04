8e1ddfa7bd842abdde735fa0907ce445159c3581
Petri Hoskonen <route.electrique@gmail.com>
2020-04-04 08:38:15 +0300

    Make enemy shoot back.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,6 +3,27 @@
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private float health = 100;
+    [SerializeField] private float shotCounter;
+    [SerializeField] private float minTimeBetweenShots = 0.2f;
+    [SerializeField] private float maxTimeBetweenShots = 0.5f;
+    [SerializeField] private GameObject enemyProjectile;
+    [SerializeField] private float projectileSpeed = 10f;
+
+    private void CountdownAndShoot()
+    {
+        shotCounter -= Time.deltaTime;
+        if (shotCounter <= 0)
+        {
+            Fire();
+            shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+        }
+    }
+
+    private void Fire()
+    {
+        GameObject ammo = Instantiate(enemyProjectile, transform.position, Quaternion.identity) as GameObject;
+        ammo.GetComponent<Rigidbody2D>().velocity += new Vector2(0, -projectileSpeed);
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -18,5 +39,15 @@ public class Enemy : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+    private void Start()
+    {
+        shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+    }
+
+    private void Update()
+    {
+        CountdownAndShoot();
     }
 }
